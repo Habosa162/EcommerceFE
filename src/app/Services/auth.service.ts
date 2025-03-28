f6eae6e8cb9aa0eabfdffa9ee3a5460f1fa4cabe
Habosa162 <habosa2000@hotmail.com>
2025-03-28 07:21:06 +0200

    addingEnviroment&LoginFinish

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,20 +1,34 @@
+import { ILoginUser } from '../core/models/Auth.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from './enviroment';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private LoginEndPoint = "https://localhost:7280/api/Auth/login";
-  private RegisterEndPoint = "https://localhost:7280/api/Auth/register";    
-
+  private LoginEndPoint = `${environment.apiUrl}/Auth/login`;
+  private RegisterEndPoint = `${environment.apiUrl}/Auth/register`;    
   
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  login(): void {
-    console.log('login');
+  login(LoginObj:ILoginUser): Observable<any>{
+    console.log(LoginObj);
+    return this.http.post(this.LoginEndPoint, LoginObj);
   }
   register() : void{
     console.log('register');
+  }
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+  isLoggedIn() : boolean{
+    return !!localStorage.getItem('token'); 
+  }
+  logout() : void{
+    localStorage.removeItem('token');
   }
 }
--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../Services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
