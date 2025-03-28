e111b662e2d8fa3b80e35b9fd650e9bc11bc7c72
Habosa162 <habosa2000@hotmail.com>
2025-03-28 16:04:32 +0200

    merchantLayout

---
+++ src/app/layouts/merchant-layout/merchant-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merchant-layout',
+  imports: [],
+  templateUrl: './merchant-layout.component.html',
+  styleUrl: './merchant-layout.component.css'
+})
+export class MerchantLayoutComponent {
+
+}
