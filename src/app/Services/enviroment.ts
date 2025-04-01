64dbc0128a501b62c2ea772fe2cb87a69becb44c
tahani <tahaniroshdy695@gmail.com>
2025-04-01 22:16:42 +0200

    productslistanddetails

--- src/app/Services/enviroment.ts
+++ src/app/Services/enviroment.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   apiUrl: 'https://localhost:7280/api'
-// };
-
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7280/api', // or 'http://localhost:5000/api' if not using HTTPS
-  useMockData: true
+  apiUrl: 'https://localhost:7280/api'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:7280/api', // or 'http://localhost:5000/api' if not using HTTPS
+//   useMockData: true
+// };
