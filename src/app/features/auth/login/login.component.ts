d3b8f87584097f30f327767e21ebdcc63084987e
Habosa162 <habosa2000@hotmail.com>
2025-04-01 14:23:12 +0200

    updateLoginComponent

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,18 +3,20 @@ import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../Services/auth.service';
 import { ILoginUser } from './../../../core/models/auth.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
-
+  error=false ;
+  errorMessage="Worng email or password please try again" ;
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -29,8 +31,11 @@ export class LoginComponent implements OnInit {
   }
 
   OnLogin(): void {
+    console.log(this.loginForm.value);
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
+      this.error = true;
+      this.errorMessage = "Please fill in all fields correctly.";
       return;
     }
 
@@ -46,6 +51,9 @@ export class LoginComponent implements OnInit {
       },
       error: (err) => {
         console.error('Login error:', err);
+        this.error = true;
+        this.loginForm.reset();
+        this.loginForm.markAllAsTouched() ;
       }
     });
   }
