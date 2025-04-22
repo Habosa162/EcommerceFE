2b3282dc748cdb8a05db9033e1e78dbfea9ca10d
karimKhaled <karimKhaled881@gmail.com>
2025-04-22 17:56:27 +0200

    sa

---
+++ src/app/core/models/admin-user.model.ts
@@ -0,0 +1,10 @@
+export class AdminUser {
+    id!: string;
+    FirstName!: string;
+    LastName!: string;
+    email!: string;
+    isActive!: boolean;
+    createdAt!: Date;
+    birthDate!: Date;
+
+}
